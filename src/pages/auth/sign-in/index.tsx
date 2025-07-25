3c6742a42dff4c33891a7c1acb8102aab4c669dc
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-07-25 16:23:08 -0300

    feat: create login page components

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -1,11 +1,35 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button, Input, Label } from '@/components'
+
 export const SignIn = () => {
   return (
-    <div>
+    <>
       <Helmet title="Login" />
 
-      <h1>SignIn</h1>
-    </div>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Insira suas credenciais para acessar o painel
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
