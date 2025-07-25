857d8460519122c553d3c68f114d3a68cec5b933
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-07-25 13:28:24 -0300

    chore: setup react hemlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router'
 
 import { router } from '@/routes'
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
       <RouterProvider router={router} />
-    </div>
+    </HelmetProvider>
   )
 }
