56c8b74caba02d7ca4be2eeecf8778d0ebc65eb9
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-07-31 18:54:15 -0300

    feat(dashboard): create revenue chart

--- src/pages/app/dashboard/dashboard/index.tsx
+++ src/pages/app/dashboard/dashboard/index.tsx
@@ -4,13 +4,14 @@ import { DayOrdersAmountCard } from '../day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from '../month-canceled-orders-amount'
 import { MonthOrdersAmountCard } from '../month-orders-amount-card'
 import { MonthRevenueCard } from '../month-reveneu-card'
+import { RevenueChart } from '../revenue-chart'
 
 export const Dashboard = () => {
   return (
-    <div>
+    <>
       <Helmet title="Dashboard" />
 
-      <div className="flx flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
         <div className="grid grid-cols-4 gap-4">
@@ -19,7 +20,11 @@ export const Dashboard = () => {
           <DayOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
         </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
