a73ef1313ab3cc77cba222d0de4e28917c6e3de0
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-08-01 06:20:38 -0300

    chore: setup API client

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from '@/components'
+import { queryClient } from '@/lib'
 import { router } from '@/routes'
-
-import { ThemeProvider } from './components'
 
 export function App() {
   return (
@@ -12,7 +13,9 @@ export function App() {
       <ThemeProvider storageKey="pizza-shop-theme" defaultTheme="dark">
         <Toaster richColors />
         <Helmet titleTemplate="%s | Pizza Shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
